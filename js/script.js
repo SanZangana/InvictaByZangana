1ef2053287ffc52f52f1586333391bf3b3279d6a
SanZangana <San9@live.se>
2023-01-13 06:40:02 +0000

    Add variables for form on account.html page

---
+++ js/script.js
@@ -0,0 +1,4 @@
+var nameError = document.getElementById('name-error')
+var emailError = document.getElementById('email-error')
+var messageError = document.getElementById('message-error')
+var submitError = document.getElementById('submit-error')

104e91072f0b6c1361f162b13c877dcff15d7b95
SanZangana <San9@live.se>
2023-01-13 06:40:36 +0000

    Add function for validating name

--- js/script.js
+++ js/script.js
@@ -2,3 +2,19 @@ var nameError = document.getElementById('name-error')
 var emailError = document.getElementById('email-error')
 var messageError = document.getElementById('message-error')
 var submitError = document.getElementById('submit-error')
+
+//  function for email validation
+function validateName() {
+  var name = document.getElementById('contact-name').value;
+
+  if(name.length == 0) {
+    nameError.innerHTML = 'Name is required';
+    return false;
+  }
+  if(!name.match(/^[a-zA-Z]+ [a-zA-Z]+$/)) {
+    nameError.innerHTML = 'Enter full name please';
+    return false;
+  }
+  nameError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+  return true;
+}

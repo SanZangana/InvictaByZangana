fa7a43ebec84d3c6a203decde255ad0f908d49d5
SanZangana <San9@live.se>
2023-01-13 06:41:11 +0000

    Add function for validating email

--- js/script.js
+++ js/script.js
@@ -18,3 +18,20 @@ function validateName() {
   nameError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
   return true;
 }
+// function for validating email
+function validateEmail() {
+    var email = document.getElementById('contact-email').value;
+  
+    if(email.length == 0) {
+      emailError.innerHTML = "Email is required"
+      return false;
+    }
+    if(!email.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)){
+      emailError.innerHTML = 'Email Invalid'
+      return false;
+    }
+    
+    emailError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
+  }
+  

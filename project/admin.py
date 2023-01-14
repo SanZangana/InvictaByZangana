9aaab5390c65130eff88240c499ab5c0bdfc609e
SanZangana <San9@live.se>
2023-01-14 15:02:56 +0000

    Add summernote to admin

--- project/admin.py
+++ project/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Review
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Review)
+class ReviewAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('content')
 
 admin.site.register(Review)
 

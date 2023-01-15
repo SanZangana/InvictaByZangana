f10aa24dfc922b6de58587cd7787343cba9770e7
SanZangana <San9@live.se>
2023-01-15 12:53:35 +0000

    Add allauth to installed apps

--- saninvicta/urls.py
+++ saninvicta/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
+    path('', include('project.urls'), name='project_urls'),
+    path('accounts/', include('allauth.urls')),
 ]

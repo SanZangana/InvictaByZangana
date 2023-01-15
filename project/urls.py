03c6c37c708923a7d5d58c9b4d2964e38bf2b516
SanZangana <San9@live.se>
2023-01-15 19:33:20 +0000

    Fix typo

--- project/urls.py
+++ project/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.ReviewList.as_view(), name='home'),
-    path('<slug:slug>/', views.ReviewDetail.as_view(), name='review_detail'),
+    path('<slug:slug>/', views.ReviewDetail.as_view(), name='review_test'),
 ]

b45ca22aff769b06a8ebd2236dc75ce01b3cde18
SanZangana <San9@live.se>
2023-01-15 08:19:15 +0000

    Add class for review and for comment

--- project/admin.py
+++ project/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
-from .models import Review
+from .models import Review, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Review)
 class ReviewAdmin(SummernoteModelAdmin):
 
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
     summernote_fields = ('content')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'review', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')

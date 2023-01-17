8d8ae77aa41844c9b09e7f52a88de165ec0fd574
SanZangana <San9@live.se>
2023-01-17 10:42:54 +0000

    Add code comment

--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,7 @@
 from . import views
 from django.urls import path
 
-
+# Difference between this urls.py and the other urls.py in 'saninvicta' foldeR?
 urlpatterns = [
     path('', views.home, name='home'),
     path('membership/', views.ReviewList.as_view(), name='membership'),

5e5b6e337ef14e8f5608305cd4b1dbd018eff2ce
SanZangana <San9@live.se>
2023-01-18 12:55:27 +0000

    Add path to 'todo' folder

--- saninvicta/urls.py
+++ saninvicta/urls.py
@@ -1,28 +1,12 @@
-"""saninvicta URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-from todo.views import get_todo_list, add_item
+from todo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include('project.urls'), name='project_urls'),
+    path('todo/', include('todo.urls'), name='todo_urls'),
     path('accounts/', include('allauth.urls')),
     # Add correct name to 'name', maybe 'Leave a review' or smth
-    path('hello/', get_todo_list, name='get_todo_list'), 
-    path('add/', add_item, name='add'), 
 ]

6a2acd180a9627a4f56231cb0f9b2271e5e6ed8d
SanZangana <San9@live.se>
2023-01-20 13:50:23 +0000

    Set debug as environment variable instead of hard-coded

--- saninvicta/settings.py
+++ saninvicta/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", False)
 
 ALLOWED_HOSTS = ['saninvicta.herokuapp.com', 'localhost']
 
@@ -47,12 +47,11 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'cloudinary_storage',
-    'todo',
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
     'crispy_forms',
-    'project',
+    'reviews',
 ]
 
 SITE_ID = 1
